203cacc43e2f61cf232a7689f963c571fb29c520
Brett.Jones <kolstrike1@gmail.com>
2024-08-25 22:09:28 -0400

    Shape functions lab complete

--- JS/Labs/Shape-Functions/script.js
+++ JS/Labs/Shape-Functions/script.js
@@ -1,14 +1,35 @@
 
-function getCircleArea(radius) {
+function getAreaOfCircle(radius) {
     const area = Math.PI * radius * radius;
     return area;
 }
 
-let area = getCircleArea(5);
+let area = getAreaOfCircle(5);
 console.log(area);
 
+function getCircumferenceOfCircle(radius) {
+    const circumference = 2 * Math.PI * radius;
+    return circumference;
+}
 
+let circumference = getCircumferenceOfCircle(5);
+console.log(circumference);
 
+function getAreaOfSquare(side) {
+    const area = side * side * side * side
+    return area;
+}
+
+let sqArea = getAreaOfSquare(5);
+console.log(sqArea);
+
+function getAreaOfTriangle(base, height) {
+    const area = (base * height) / 2;
+    return area;
+}
+
+let triArea = getAreaOfTriangle(8, 3);
+console.log(triArea);
 
 
 
