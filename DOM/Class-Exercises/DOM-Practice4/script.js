6f91e0af1f74d4f4bdd514d749cbefb449e15c34
Kolstrike <kolstrike1@gmail.com>
2024-09-19 21:41:29 -0400

    CYA

---
+++ DOM/Class-Exercises/DOM-Practice4/script.js
@@ -0,0 +1,20 @@
+
+
+const userForm = document.getElementById('userForm');
+const usernameInput = document.getElementById('username');
+const passwordInput = document.getElementById('password');
+const formOutput = document.getElementById('password')
+
+
+console.log('Script loaded: ' + new )
+
+
+
+
+
+
+
+
+
+
+
--- DOM/Labs/Ready, Set, Action!/index.js
+++ DOM/Labs/Ready, Set, Action!/index.js
@@ -1,6 +1,16 @@
 
+const button = document.querySelectorAll('.class-button');
 
+//querySelectorAll = node list is like an array, each node is an element found by the querySelector
 
+//arrow function inside the forEach, 'button' is referring to the element in the array
+button.forEach( (button) => {
+
+    button.addEventListener('click', () => {
+        button.style.backgroundColor = 'lightblue';
+    })
+
+});
 
 
 
